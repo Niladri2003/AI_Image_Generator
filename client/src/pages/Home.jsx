c48531ae6f4caff007f0c389aa1ef39f3af72a2b
Niladri2003 <niladri.adak.21@aot.edu.in>
2023-05-19 23:17:17 +0530

    setup done

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Card from '../components/Card'
+import Loader from '../components/Loader'
+
+
+const Home = () => {
+    const[loading,setLoading]=useState(false);
+    const[allPosts,setAllposts]=useState(null);
+  return (
+    <div>
+      home
+    </div>
+  )
+}
+
+export default Home
